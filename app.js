fb79952b1ec21462db9833e86cbc8730b8758da5
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-08-17 19:03:37 -0500

    notes

---
+++ app.js
@@ -0,0 +1,68 @@
+// function addition(num1, num2) {
+//     return num1 + num2
+// }
+// console.log(addition(3, 5))
+
+// function hoursIntoSeconds(hours) {
+//     return hours * 3600
+// }
+
+// console.log(hoursIntoSeconds(2))
+
+// function calcPerimeter(num1, num2) {
+//         return num1 * 2 + num2 * 2
+//     }
+//     console.log(calcPerimeter(6, 7))
+
+// function calcTriangleArea(base, height) {
+//         return base / 2 * height
+//     }
+//     console.log(calcTriangleArea(10, 10))
+
+// function appendFrontend(str) {
+//         return str + 'Frontend'
+//     }
+//     console.log(appendFrontend('Apple'))
+
+// +number turns string number into an actual number.
+// function sumGreaterThan100(num1, num2) {
+//     return num1 + num2 > 100
+// }
+// console.log(sumGreaterThan100(100, 50))
+
+// function lessThanOrEqualToZero(number) {
+//     return number <= 0
+// }
+// console.log(lessThanOrEqualToZero(0))
+
+// function oppositeBoolean(boolean) {
+//     return !boolean;
+// }
+// console.log(oppositeBoolean(true))
+
+// function isNotZero(any) {
+//     return any !== 0
+// }
+// console.log(isNotZero(9))
+
+// function calcRemainder(num1, num2) {
+//     return num1 % num2
+// }
+// console.log(calcRemainder(9, 8))
+
+// function isOdd(num) {
+//     return num % 2 !== 0
+// }
+// console.log(isOdd(5))
+
+// function booleanInteger(num) {
+//     return num % 2 === 0 ? 1 : -1; // can use ternry operators in functions. 
+// }
+// console.log(booleanInteger(6))
+
+
+// function isLoggedInAndSubscribed(value, value2) {
+//     return value === 'loggedIn' && value2 === 'subscribed'
+// }
+
+// console.log(isLoggedInAndSubscribed('loggedIn', 'subscribed'))
